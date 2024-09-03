43efb579402a39c1e9837fae2e5cef06cd516a0e
Dinaol-Bedada <bedada.dinaol@gmail.com>
2024-09-03 12:20:18 +0200

    contact footer update

--- app/components/Footer/FooterTop.tsx
+++ app/components/Footer/FooterTop.tsx
@@ -6,12 +6,12 @@ const FooterTop = () => {
     <div className="flex justify-between items-center flex-col lg:flex-row px-4 md:px-8 lg:px-24 gap-8 bg-white mt-6">
       <div>
         <h1 className="font-bold text-2xl mb-4 md:text-3xl lg:text-4xl">
-          Ready to Make Your Visa?
+          Ready to apply for Visa?
         </h1>
         <p className="text-lg">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit utelit tellus
+          If you are ready to Apply for any type of visa, 
           <br />
-          luctus nec ullamcorper mattis pulvinar dapibus leo luctus nec elit.
+          Submit your application requests here.
         </p>
       </div>
       <div>
@@ -22,6 +22,15 @@ const FooterTop = () => {
           height={700}
           className="rounded-2xl w-full h-full"
         />
+        <motion.button
+          ref={buttonRef}
+          className="bg-blue-500 text-white px-4 py-2 rounded-md"
+          initial={{ scale: 0 }}
+          animate={buttonInView ? { scale: 1 } : { scale: 0 }}
+          transition={{ duration: 1, delay: 2 }}
+        >
+          SUBMIT YOUR REQUEST NOW
+        </motion.button>
       </div>
     </div>
   );
