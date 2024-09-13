831e40241924d366a6e04e3f06518b4f69b86360
Dinaol Bedada <117851922+Dinaol-Bedada@users.noreply.github.com>
2024-09-13 20:39:44 +0200

    Update FooterTop.tsx

--- app/components/Footer/FooterTop.tsx
+++ app/components/Footer/FooterTop.tsx
@@ -11,11 +11,11 @@ const FooterTop = () => {
         <p className="text-lg">
           If you are ready to Apply for any type of visa,
           <br />
-          Submit your application by clicking "Apply Now Button".
+          Submit your application by clicking "Apply Now" Button.
         </p>
         <div className="block py-2 text-white hover:bg-gray-200">
           <a
-            href="https://iglobalvisa.com/apply"
+            href="https://apply.iglobalvisa.com/"
             target="_blank"
             rel="noopener noreferrer"
           >
